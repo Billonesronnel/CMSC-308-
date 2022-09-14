d23b4ecb29f36067e10627bbf58fc07b6bf58f91
ㅤㅤㅤㅤㅤBㅤR <107450021+Billonesronnel@users.noreply.github.com>
2022-09-14 21:46:09 +0800

    Add files via upload

---
+++ Activity/Activity 1/Activity 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Activity_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] Email = {"ronnel.billones@gwapo.com",
+                              "ronnel.billones@pogi.com" ,
+                              "ronnel.billones@handsome.com" };
+            string[] Username = {"ronnelgwapo",
+                                 "ronnelpogi" ,
+                                 "ronnelhandsome"};
+            string[] Password = {"ronnelgwapo123",
+                                 "ronnelpogi456",
+                                 "ronnelhandsome789"};
+
+            Console.Write("Index : ");
+            int Index = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Email : " + Email[Index]);
+            Console.WriteLine("Username : " + Username[Index]);
+            Console.WriteLine("Password : " + Password[Index]);
+
+
+        }
+    }
+}
